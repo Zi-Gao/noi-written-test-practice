13563b3e61c440d38d3a566e81f7c6f3f04a7911
Zachary Gao <gyc.zachary@gmail.com>
2025-07-11 21:27:22 +0800

    add disclaimer

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,6 +6,8 @@
       </div>
       <p>这是一个帮助你记忆 NOI 笔试题库的网站。</p>
       <p>祝所有参赛者 NOI 2025 rp++！</p>
+      <p>免责声明：不保证题库无错，版权所属CCF，如需使用请一定自行对照<a href="https://noi.cn/xw/2025-06-16/844453.shtml">CCF下发的2025年题库</a>避免对您的考试造成影响</p>
+      
     </div>
     <div class="d-grid gap-2">
       <router-link to="/test" class="btn btn-primary btn-lg">
